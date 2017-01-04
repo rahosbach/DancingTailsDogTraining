0a3bee634a071d4a8e6b107406631739be5b2a3b
Robert Hosbach <robert.hosbach@gmail.com>
2017-01-03 18:20:11 -0800

    Updated javascript files using Google PageSpeed Insights optimized files (automatically downloaded).

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,18 +1,1 @@
-'use strict';
-
-/* Filters */
-
-angular.module('myApp.filters', [])
-	// .filter('interpolate', function (version) {
-	// 	return function (text) {
-	// 		return String(text).replace(/\%VERSION\%/mg, version);
-	// 	};
-	// })
-	.filter('slice', function(){
-	  	return function(arr,start,end){
-	  		if (!arr){
-	  			return;
-	  		}
-	  		return arr.slice(start,end);
-	  	};
-  });
+'use strict';angular.module('myApp.filters',[]).filter('slice',function(){return function(arr,start,end){if(!arr){return;}return arr.slice(start,end);};});
